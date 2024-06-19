fd3fdee0eb9fd067603f04f6f698fec3816cb7b3
chimdice <118031439+chimdice@users.noreply.github.com>
2024-06-19 14:48:24 -0700

    Fix tail method add pop and contains method

--- link.js
+++ link.js
@@ -69,6 +69,10 @@ class LinkList {
     tail () {
         let tempList = this.linkList.link;
 
+        if ((typeof tempList === 'undefined')||(tempList === null)) {
+            return;
+        }
+
         while (! (tempList.link === null)) {
             tempList = tempList.link;
         };
@@ -76,10 +80,70 @@ class LinkList {
         return tempList;
     };
 
+    at (index) {
+        let loc = 0;
+        let tempList = this.linkList.link;
+
+        while (loc < index) {
+            if (tempList.link === null) {
+                return "Index out of Range";
+            } else {
+                tempList = tempList.link;
+            };
+
+            loc += 1
+        };
+
+        return tempList;
+    };
+
+    pop () {
+        let tempList = this.linkList.link;
+        let parentList = this.linkList;
+        
+        if ((typeof tempList === 'undefined')||(tempList === null)) {
+            return;
+        };
+
+        while (! (tempList.link === null)) {
+            tempList = tempList.link;
+            parentList = parentList.link;
+        };
+
+        delete parentList.link;
+        parentList.link = null;
+    };
+
+    contains (value) {
+        let tempList = this.linkList.link;
+        let inList = false
+
+        if ((typeof tempList === 'undefined')||(tempList === null)) {
+            return inList;
+        }
+
+        while (! inList) {
+            if (tempList.value === value) {
+                inList = true;
+            } else if (tempList.link === null) {
+                break;
+            } else {
+                tempList = tempList.link;
+            };
+        };
+
+        return inList;
+    };
+
+
+    find (value) {
+        
+    }
 };
 
 const link = new LinkList();
 link.append(3);
 link.prepend(12);
 link.append(45);
-console.log(link.tail());
+console.log(link.linkList)
+console.log(link.contains(45));
